80d5c7defc6a42633024aaf047f3e8b1bb5bb1e4
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2026-03-28 13:36:45 +0900

    feat: ESLint と Prettier のセットアップ

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +22,6 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+
+  prettier,
+]);
